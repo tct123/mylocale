d3deb24332e73a5c0bc0bdab96c39c244ee1cfc1
tct123 <42028373+tct123@users.noreply.github.com>
2024-03-10 20:11:14 +0100

    .

--- mylocale/read.py
+++ mylocale/read.py
@@ -1,3 +1,9 @@
-def read(fp, stringname, variables=[]):
+import csv
+def read(fp: str, stringname: str, variables:list=[]):
     with open(file=fp, mode="r") as f:
         content = f.read()
+    return content
+
+
+if __name__ == "__main__":
+    print(read(fp="localisation.csv",))
