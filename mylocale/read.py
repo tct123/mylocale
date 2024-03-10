7016761d07397b08ecbda9f391395bf39b026214
tct123 <42028373+tct123@users.noreply.github.com>
2024-03-10 20:44:37 +0100

    Update read.py

--- mylocale/read.py
+++ mylocale/read.py
@@ -1,9 +1,13 @@
 import csv
-def read(fp: str, stringname: str, locale:str, variables:list=[]): # fp for path to translationfile, locale for wanted locale and variables for eventual variables
+
+
+def read(
+    fp: str, stringname: str, locale: str, variables: list = []
+):  # fp for path to translationfile, locale for wanted locale and variables for eventual variables
     with open(file=fp, mode="r") as f:
         content = f.read()
     return content
 
 
 if __name__ == "__main__":
-    print(read(fp="localisation.csv",))
+    print(read(fp="localisation.csv", stringname="HELLOWORLD", locale="en_EN"))
