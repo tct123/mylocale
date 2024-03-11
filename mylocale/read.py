be927d7034161c6be34c63d338ef7b24e8d0f0a7
tct123 <42028373+tct123@users.noreply.github.com>
2024-03-11 01:37:03 +0100

    Update read.py

--- mylocale/read.py
+++ mylocale/read.py
@@ -4,17 +4,17 @@ import csv
 def read(
     fp: str, stringname: str, locale: str, variables: list = []
 ):  # fp for path to translationfile, locale for wanted locale and variables for eventual variables
-    mylistforindex = []
+    KEYS = []
+    STRINGS = []
     with open(file=fp, newline="", encoding="utf-8", mode="r") as csvfile:
         csvreader = csv.DictReader(csvfile, delimiter=",")
         for item in csvreader:
-            mylistforindex.append(item[stringname])
-            
+            KEYS.append(item[stringname])
+        for item in csvreader:
+            STRINGS.append(item["de_DE"])
 
-        return mylistforindex
-        
-
+        return STRINGS
 
 
 if __name__ == "__main__":
-    print(read(fp="localisation.csv", stringname="stringname", locale="en_EN"))
+    print(read(fp="localisation.csv", stringname="stringname", locale="de_de"))
