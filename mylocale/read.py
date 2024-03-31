cd9293929749ca0ecd3e38e0608e3e4c42415dfc
tct123 <42028373+tct123@users.noreply.github.com>
2024-03-31 20:58:01 +0200

    Update read.py

--- mylocale/read.py
+++ mylocale/read.py
@@ -4,16 +4,17 @@ import csv
 def read(
     fp: str, stringname: str, locale: str, variables: list = []
 ):  # fp for path to translationfile, locale for wanted locale and variables for eventual variables
-    KEYS = []
-    STRINGS = []
-    with open(file=fp, newline="", encoding="utf-8", mode="r") as csvfile:
-        csvreader = csv.DictReader(csvfile, delimiter=",")
-        for item in csvreader:
-            KEYS.append(item[stringname])
-        for item in csvreader:
-            STRINGS.append(item["de_DE"])
-
-        return STRINGS
+    # KEYS = []
+    # STRINGS = []
+    # with open(file=fp, newline="", encoding="utf-8", mode="r") as csvfile:
+    #    csvreader = csv.DictReader(csvfile, delimiter=",")
+    #    for item in csvreader:
+    #        KEYS.append(item[stringname])
+    #    for item in csvreader:
+    #        STRINGS.append(item["de_DE"])
+    #
+    #    return STRINGS
+    pass
 
 
 if __name__ == "__main__":
