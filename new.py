82d9144f1351575bbed0c057257e172c3291c668
tct123 <42028373+tct123@users.noreply.github.com>
2024-04-03 03:08:42 +0200

    Update new.py

--- new.py
+++ new.py
@@ -3,11 +3,10 @@ from mylocale.TR import tr
 var1 = 1
 var2 = 2
 var3 = var1 + var2
-print(
-    tr(
-        csv_file="examples/localisation.csv",
-        target_key="TEXTVARS",
-        varnames=["VAR1", "VAR2", "VAR3"],
-        vars=[var1, var2, var3],
-    )
+x = tr(
+    csv_file="examples/localisation.csv",
+    target_key="TEXTVARS",
+    varnames=["VAR1", "VAR2", "VAR3"],
+    vars=[var1, var2, var3],
 )
+print(x)
