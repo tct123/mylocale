1bc7a294f52dd7b715d590ab25f8cd7040018c24
tct123 <42028373+tct123@users.noreply.github.com>
2024-04-03 02:19:51 +0200

    .

--- mylocale/TR.py
+++ mylocale/TR.py
@@ -1,7 +1,8 @@
 import csv
+from babel import default_locale
 
 
-def tr(csv_file, target_key, langcode):
+def tr(csv_file, target_key, varnames:list=[],vars:list=[], langcode=default_locale()):
     f = open(csv_file, newline="")
     locale_csv = csv.DictReader(f=f, delimiter=",")
     for item in locale_csv:
