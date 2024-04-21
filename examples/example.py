0eeb025026e5935779a8b0a830f051a34531833c
tct123 <42028373+tct123@users.noreply.github.com>
2024-04-21 17:12:47 +0200

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -2,5 +2,4 @@ from mylocale.TR import tr
 
 csv_file = "localisation.csv"
 target_key = "HELLOWORLD"
-langcode = "en_EN"
-print(tr(csv_file=csv_file, target_key=target_key, langcode=langcode))
+print(tr(csv_file=csv_file, target_key=target_key))
