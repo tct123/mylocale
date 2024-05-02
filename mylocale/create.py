b862294fca300241a1e3d33ea3c20c185abef667
tct123 <42028373+tct123@users.noreply.github.com>
2024-05-02 06:09:28 +0200

    Update create.py

--- mylocale/create.py
+++ mylocale/create.py
@@ -1,8 +1,7 @@
-
 def create():
     string = "stringname,en_EN\nHELLOWORLD,Hello World"
     with open(file="localisation.csv", mode="w") as f:
         f.write(string)
 
-if __name__ == "__main__":
-    create()
+
+create()
