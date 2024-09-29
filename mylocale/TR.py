53037b56df5c2e51b9a4cb9d6ee650bdcf507165
tct123 <42028373+tct123@users.noreply.github.com>
2024-09-29 11:45:47 +0200

    Update TR.py

--- mylocale/TR.py
+++ mylocale/TR.py
@@ -6,7 +6,7 @@ def tr(
     csv_file,
     target_key,
     langcode=locale.getlocale()[0].split("_")[0],
-) -> str:
+):
     f = open(csv_file, newline="")
     locale_csv = csv.DictReader(f=f, delimiter=",")
     for item in locale_csv:
