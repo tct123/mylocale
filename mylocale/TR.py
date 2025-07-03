d1097e589d7e75215eb467a34db1df875609ec0f
tct123 <42028373+tct123@users.noreply.github.com>
2025-07-03 09:20:07 +0200

    Update TR.py

--- mylocale/TR.py
+++ mylocale/TR.py
@@ -2,9 +2,19 @@ import csv
 import locale
 
 
+# TODO: add rtl support
+def check_rtl(langcode):
+    rtl_langcodes = []
+    if langcode in rtl_langcodes:
+        return True
+    else:
+        return False
+
+
 def tr(csv_file, target_key, langcode):
     f = open(csv_file, newline="")
     locale_csv = csv.DictReader(f=f, delimiter=",")
+    print(locale_csv.fieldnames)
     for item in locale_csv:
         if item["stringname"] == target_key:
             try:
